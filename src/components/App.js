e42c94a8ec83c798238110c686499cf6e1d6e34a
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-08-11 10:36:00 -0500

    Install fomantic ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
-
 import React from 'react';
+
+import '../assets/fomantic/dist/semantic.css';
+import { Container, Header } from 'semantic-ui-react';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>App</div>
-    )
+      <Container>
+        <Header>App</Header>
+      </Container>
+    );
   }
 }
