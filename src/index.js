eefb11c240207be3dcd7d5a1edeac5567683aab4
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-08-11 14:00:04 -0500

    Add redux skeleton

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+// React imports
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Redux imports
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import reducers from './reducers';
+
 import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
