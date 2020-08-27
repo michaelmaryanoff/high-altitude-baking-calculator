527059ed42bf16bb8f819f9e4ec04847b257ba5a
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-08-27 14:35:50 -0500

    Clean up imports

--- src/components/form/CalculatorForm.js
+++ src/components/form/CalculatorForm.js
@@ -2,68 +2,24 @@
  * React imports
  */
 
-import React, { useState, useEffect } from 'react';
-
-import { defaultUnit } from '../../constants';
+import React from 'react';
 
 /**
  * Redux imports
  */
-import { clearForm, calculateOutputs, handleCustomaryInput } from '../../actions';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 /**
  * Component imports
  */
-import BakingTempField from './BakingTempField';
-import BakingTimeField from './BakingTimeField';
-import CupsAndTbspField from './CupsAndTbspField';
-import TspField from './TspField';
-import FieldRow from './FieldWrapper';
-import ClearButton from './ClearButton';
-import CalculateButton from './CalculateButton';
-import ButtonWrapper from './ButtonWrapper';
-import AltitudeField from './AltitudeField';
 
 import CustomaryCalcForm from './CustomaryCalcForm';
-import MetricCalcForm from './MetricCalcForm'
+import MetricCalcForm from './MetricCalcForm';
 
 /**
- * Constants
+ * Base calculation compoent that will render the approprate calculation form
+ * depending on which units are selected by the user
  */
-
-const initialState = {
-  unitFieldEnabled: false,
-  unitInput: defaultUnit,
-  altitudeInput: '',
-  ovenTempInput: '',
-  ovenTempOutput: '',
-  bakingTimeInput: '',
-  bakingMinsInput: '',
-  bakingHoursInput: '',
-  bakingTimeOutput: '',
-  liquidCupsInput: '',
-  liquidPartialCupInput: '',
-  liquidTbspInput: '',
-  liquidOutput: '',
-  flourCupsInput: '',
-  flourTbspInput: '',
-  flourPartialCupInput: '',
-  flourOutput: '',
-  sugarCupsInput: '',
-  sugarTbspInput: '',
-  sugarPartialCupInput: '',
-  sugarOutput: '',
-  bakingPowderInput: '',
-  bakingPowderTspInput: '',
-  bakingPowderPartialTspInput: '',
-  bakingPowderOutput: '',
-
-  yeastTspInput: '',
-  yeastPartialTspInput: '',
-  yeastOutput: ''
-};
-
 const CalculatorForm = () => {
   const { unit } = useSelector(state => state.calculationForm);
   return unit === 'customary' ? <CustomaryCalcForm /> : <MetricCalcForm />;
