304e2dbb84bfd4b3f84fec46ab44225bbe3cac36
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-08-27 15:57:35 -0500

    Set up metric reducers

--- src/reducers/calculationFormReducerMetric.js
+++ src/reducers/calculationFormReducerMetric.js
@@ -1,13 +1,52 @@
-import { SET_ALTITUDE_METERS, SET_OVEN_TEMP_C, CLEAR_FORM } from '../actions/metricTypes';
+import {
+  SET_ALTITUDE_METERS,
+  SET_OVEN_TEMP_C,
+  SET_BAKING_TIME,
+  SET_BAKING_MINS,
+  SET_BAKING_HOURS,
+  SET_LIQUID_GRAMS,
+  SET_FLOUR_GRAMS,
+  SET_SUGAR_GRAMS,
+  SET_BAKING_POWDER_GRAMS,
+  SET_YEAST_GRAMS,
+  CLEAR_FORM
+} from '../actions/metricTypes';
 
 const INTIAL_STATE = {
-  altitude: 0
+  altitude: 0,
+  ovenTempCelciusSet: 0,
+  bakingTimeSet: 0,
+  bakingMinsSet: 0,
+  bakingHoursSet: 0,
+  liquidGramsSet: 0,
+  flourGramsSet: 0,
+  sugarGramsSet: 0,
+  bakingPowderGramsSet: 0,
+  yeastGramsSet: 0
 };
 
 export default (state = INTIAL_STATE, action) => {
   switch (action.type) {
     case SET_ALTITUDE_METERS:
       return { ...state, altitude: action.payload };
+    case SET_OVEN_TEMP_C:
+      return { ...state, ovenTempCelciusSet: action.payload };
+    case SET_BAKING_TIME:
+      return { ...state, bakingTimeSet: action.payload };
+    case SET_BAKING_MINS:
+      return { ...state, bakingMinsSet: action.payload };
+    case SET_BAKING_HOURS:
+      return { ...state, bakingHoursSet: action.payload };
+    case SET_LIQUID_GRAMS:
+      return { ...state, liquidGramsSet: action.payload };
+    case SET_FLOUR_GRAMS:
+      return { ...state, flourGramsSet: action.payload };
+    case SET_SUGAR_GRAMS:
+      return { ...state, sugarGramsSet: action.payload };
+    case SET_BAKING_POWDER_GRAMS:
+      return { ...state, bakingPowderGramsSet: action.payload };
+    case SET_YEAST_GRAMS:
+      return { ...state, yeastGramsSet: action.payload };
     case CLEAR_FORM:
       return { ...state, ...INTIAL_STATE };
     default:
---
+++ src/reducers/calculationOutputReducerMetric.js
@@ -0,0 +1,81 @@
+import {
+  CALCULATE_MIN_OVEN_TEMP,
+  CALCULATE_MAX_OVEN_TEMP,
+  CALCULATE_MIN_TIME,
+  CALCULATE_MAX_TIME,
+  CALCULATE_BAKING_POWDER_GRAMS,
+  CALCULATE_YEAST_GRAMS,
+  CALCULATE_FLOUR_GRAMS,
+  CALCULATE_MIN_LIQUID_GRAMS,
+  CALCULATE_MAX_LIQUID_GRAMS,
+  CALCULATE_SUGAR_GRAMS,
+  SET_DISPLAY_TEMP_C,
+  SET_DISPLAY_TIME,
+  SET_DISPLAY_FLOUR_GRAMS,
+  SET_DISPLAY_LIQUID_GRAMS,
+  SET_DISPLAY_SUGAR_GRAMS,
+  SET_DISPLAY_YEAST_GRAMS,
+  SET_DISPLAY_BAKING_POWDER_GRAMS
+} from '../actions/metricTypes';
+
+const INITIAL_STATE = {
+  results: null,
+  minOvenTempCalc: 0,
+  maxOvenTempCalc: 0,
+  minTimeCalc: 0,
+  maxTimeCalc: 0,
+  bakingPowderGramsCalc: 0,
+  yeastGramsCalc: 0,
+  flourGramsCalc: 0,
+  minLiquidGramsCalc: 0,
+  macLiquidGramsCalc: 0,
+  sugarGramsCalc: 0,
+  displayTempCelsiusCalc: 0,
+  displayTime: '',
+  displayFlourGrams: '',
+  displayLiquidGrams: '',
+  displaySugarGrams: '',
+  displayYeastGrams: '',
+  diplayBakingPowderGrams: ''
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case CALCULATE_MIN_OVEN_TEMP:
+      return { ...state, minOvenTempCalc: action.payload };
+    case CALCULATE_MAX_OVEN_TEMP:
+      return { ...state, maxOvenTempCalc: action.payload };
+    case CALCULATE_MIN_TIME:
+      return { ...state, minTimeCalc: action.payload };
+    case CALCULATE_MAX_TIME:
+      return { ...state, maxTimeCalc: action.payload };
+    case CALCULATE_BAKING_POWDER_GRAMS:
+      return { ...state, bakingPowderGramsCalc: action.payload };
+    case CALCULATE_YEAST_GRAMS:
+      return { ...state, yeastGramsCalc: action.payload };
+    case CALCULATE_FLOUR_GRAMS:
+      return { ...state, flourGramsCalc: action.payload };
+    case CALCULATE_MIN_LIQUID_GRAMS:
+      return { ...state, minLiquidGramsCalc: action.payload };
+    case CALCULATE_MAX_LIQUID_GRAMS:
+      return { ...state, macLiquidGramsCalc: action.payload };
+    case CALCULATE_SUGAR_GRAMS:
+      return { ...state, sugarGramsCalc: action.payload };
+    case SET_DISPLAY_TEMP_C:
+      return { ...state, displayTempCelsiusCalc: action.payload };
+    case SET_DISPLAY_TIME:
+      return { ...state, displayTime: action.payload };
+    case SET_DISPLAY_FLOUR_GRAMS:
+      return { ...state, displayFlourGrams: action.payload };
+    case SET_DISPLAY_LIQUID_GRAMS:
+      return { ...state, displayLiquidGrams: action.payload };
+    case SET_DISPLAY_SUGAR_GRAMS:
+      return { ...state, displaySugarGrams: action.payload };
+    case SET_DISPLAY_YEAST_GRAMS:
+      return { ...state, displayYeastGrams: action.payload };
+    case SET_DISPLAY_BAKING_POWDER_GRAMS:
+      return { ...state, diplayBakingPowderGrams: action.payload };
+    default:
+      return state;
+  }
+};
