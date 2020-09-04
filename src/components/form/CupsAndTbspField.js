0856c5c5a8d826d834e0796983239d4ebf8f78b2
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-09-04 13:32:18 -0500

    Add three quarter cup value

--- src/components/form/CupsAndTbspField.js
+++ src/components/form/CupsAndTbspField.js
@@ -12,7 +12,8 @@ const partialCupDropDownDataSource = [
   { label: '1/4', value: 0.25 },
   { label: '1/3', value: 0.33333 },
   { label: '1/2', value: 0.5 },
-  { label: '2/3', value: 0.66666 }
+  { label: '2/3', value: 0.66666 },
+  { label: '3/4', value: 0.75 }
 ];
 
 const CupsAndTbspField = props => {
